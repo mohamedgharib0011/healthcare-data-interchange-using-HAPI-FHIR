ae25172bbd130d16ca6eaa4bcafb632905c4238e
Mohamed Gharib <mgharib@g2o.com>
2020-05-27 15:36:35 -0400

    added hapi-fhir-ui

---
+++ hapi-fhir-ui/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { ApiCallService } from './api-call.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  patients:any;
+  title = 'hapi-fhir-ui';
+   constructor(private apiCall:ApiCallService){
+    apiCall.searchPatients().subscribe(res=>{
+      this.patients=res.entry;
+      console.log(this.patients[0]);
+    });
+  }
+
+
+}
